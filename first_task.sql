c9a903516bb319278846bce3c845e848fe3573c7
Aleksandr Gorbanev <59090915+alexgorx@users.noreply.github.com>
2023-06-06 14:43:22 +0300

    Update first_task.sql
    edit01

--- first_task.sql
+++ first_task.sql
@@ -1,13 +1,16 @@
+-- 1. Написать запрос для вывода пользователей, которые не создали ни одного
+-- поста и не оставили ни одного коммента. (несколько вариантов запроса)
 
-1. Первый и самый неоптимальный вариант
+-- 1. Первый и самый неоптимальный вариант
 SELECT 
   DisplayName
 FROM users
 WHERE Id NOT in (SELECT DISTINCT OwnerUserId FROM posts)
   AND Id NOT in (SELECT DISTINCT UserID FROM comments)
 
-2.
-SELECT DisplayName
+-- 2.
+SELECT 
+  DisplayName
 FROM users t1
 JOIN 
   (SELECT Id FROM users
@@ -17,7 +20,7 @@ JOIN
   SELECT DISTINCT(UserID) AS Id FROM comments) t2
   ON t1.Id = t2.id
 
-3.
+-- 3.
 SELECT 
   DisplayName
 FROM users t1
@@ -29,7 +32,8 @@ WHERE NOT EXISTS (
     (SELECT UserId
     FROM comments t3
     WHERE t1.Id = t3.UserID)
-4.
+    
+-- 4.
 SELECT 
   DisplayName
 FROM users t1
