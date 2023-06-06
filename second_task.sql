1cb7a851480ac76c324f2720e5da08f0ba95aa51
Aleksandr Gorbanev <59090915+alexgorx@users.noreply.github.com>
2023-06-06 15:00:17 +0300

    Create second_task.sql
    counting posts and comments

---
+++ second_task.sql
@@ -0,0 +1,44 @@
+--Написать запрос для вывода года, кол-ва постов за год и кол-во комментов за год.
+
+SELECT
+  t1.year AS year,
+  t1.post_cnt, AS post_counts,
+  t2.comment_cnt AS comment_counts
+FROM
+    (SELECT
+      DATE_PART('year', CreationDate) AS year,
+      COUNT(Id) AS post_cnt
+    FROM posts
+    WHERE CreationDate BETWEEN (current_date - '1 year'::interval) AND current_date
+    GROUP BY year) t1
+    JOIN
+    (SELECT 
+      DATE_PART('year', CreationDate) AS year,
+      COUNT(Id) AS comment_cnt
+    FROM comments
+    WHERE CreationDate BETWEEN (current_date - '1 year'::interval) AND current_date
+    GROUP BY year) t2
+    ON t1.year = t2.year
+  
+  -- если предусматривать возможность отсутствия публикаций 
+  -- или комментариев за указанный отрезок времени, запрос будет следующим:
+  
+  SELECT
+  COALESCE(t1.year,t2.year) AS year,
+  COALESCE(t1.post_cnt,0) AS post_counts,
+  COALESCE(t2.comment_cnt,0) AS comment_counts
+FROM
+    (SELECT
+      DATE_PART('year', CreationDate) AS year,
+      COUNT(Id) AS post_cnt
+    FROM posts
+    WHERE CreationDate BETWEEN (current_date - '1 year'::interval) AND current_date
+    GROUP BY year) t1
+    FULL JOIN
+    (SELECT 
+      DATE_PART('year', CreationDate) AS year,
+      COUNT(Id) AS comment_cnt
+    FROM comments
+    WHERE CreationDate BETWEEN (current_date - '1 year'::interval) AND current_date
+    GROUP BY year) t2
+    ON t1.year = t2.year
