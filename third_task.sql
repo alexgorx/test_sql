51266e8ce17d0b83d5cc965ded5a33a41549f392
Aleksandr Gorbanev <59090915+alexgorx@users.noreply.github.com>
2023-06-06 15:04:21 +0300

    Create third_task.sql
    Top-3 commentators

---
+++ third_task.sql
@@ -0,0 +1,16 @@
+-- Вывести 3 самых активных (по кол-ву комментариев) пользователей (display
+-- name) и их кол-во комментариев
+
+SELECT 
+  t1.DisplayName,
+  t2.comment_count
+FROM Users t1
+JOIN 
+  (SELECT 
+    UserID,
+    COUNT(Id) AS comment_count
+  FROM comments
+  GROUP BY UserID
+  ORDER BY comment_count DESC
+  LIMIT 3) t2
+  ON t1.Id = t2.UserID
